992fdd3e728f14f4053b22d571021c445e12a242
Mike <fishdawg@gmail.com>
2025-02-22 00:22:02 -0800

    Update slitheriobigmousecursor.js

--- slitheriobigmousecursor.js
+++ slitheriobigmousecursor.js
@@ -5,31 +5,30 @@
 // @description  Makes the mouse cursor big and easier to see.
 // @author       FishDawg
 // @match        http*://slither.io/
+// @match        http*://slither.com/io
 // @grant        none
 // @compatible   Greasemonkey
-// @downloadURL  https://github.com/FishDawg/SlitherioServerLock/raw/master/slitherioserverlock.js
+// @downloadURL  https://github.com/FishDawg/SlitherioBigMouseCursor/raw/master/slitheriobigmousecursor.js
 // ==/UserScript==
 
 (function(){
   var cursorBox;
-  
+
   function updatePosition(event) {
     event = event || window.event;
     if (typeof event.clientX != "undefined") {
-      xm = event.clientX - ww / 2;
-      ym = event.clientY - hh / 2;
       cursorBox.style.left = event.clientX - cursorBox.offsetWidth / 2;
       cursorBox.style.top = event.clientY - cursorBox.offsetHeight / 2;
     }
   }
-  
+
   function disableScrolling() {
     document.body.style.overflow = 'hidden';
   }
-  
+
   function createCursorBox() {
     cursorBox = document.createElement('div');
-    
+
     cursorBox.id = 'cursor-box';
     cursorBox.style.position = 'absolute';
     cursorBox.style.left = '-1000px';
@@ -43,19 +42,17 @@
     cursorBox.style.borderRadius = '40%';
     cursorBox.style.zIndex = 2000;
     cursorBox.style.pointerEvents = 'none';
-    
+
     document.body.appendChild(cursorBox);
   }
-  
+
   function initialize() {
     disableScrolling();
     createCursorBox();
-    
+
     document.body.style.cursor = 'all-scroll';
-    window.onmousemove = updatePosition;
-    
-    //document.querySelector('body > canvas.nsi').requestPointerLock();
+    window.addEventListener('mousemove', updatePosition);
   }
-  
-  window.onload = initialize;
+
+  window.addEventListener('load', initialize);
 })();
